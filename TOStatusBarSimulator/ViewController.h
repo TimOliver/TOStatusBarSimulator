e5a749de430ec11a14a5ecb6d9b4bd364674499c
Tim Oliver <me@timoliver.com.au>
2017-05-02 00:24:03 -0700

    Initial Xcode project

---
+++ TOStatusBarSimulator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TOStatusBarSimulator
+//
+//  Created by Tim Oliver on 5/2/17.
+//  Copyright © 2017 Timothy Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TOStatusBarSimulator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TOStatusBarSimulator
+//
+//  Created by Tim Oliver on 5/2/17.
+//  Copyright © 2017 Timothy Oliver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
